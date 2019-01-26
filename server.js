b283891148eb5d2dbd04b78d57233f8f3c9c8d04
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-01-26 23:02:14 +0530

    retrieve values

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Details = require('./app/models/cash');
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Details = require("./app/models/cash");
 var MongoClient = require("mongodb").MongoClient;
 
 // Configure app for bodyParser()
 // lets us grab data from the body of POST
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 
 // Set up port for server to listen on
@@ -26,41 +28,41 @@ MongoClient.connect(
     var router = express.Router();
 
     // Routes will all be prefixed with /API
-    app.use('/api', router);
+    app.use("/api", router);
 
     //MIDDLE WARE-
     router.use(function (req, res, next) {
-      console.log('FYI...There is some processing currently going down');
+      console.log("FYI...There is some processing currently going down");
       next();
     });
 
     // test route
-    router.get('/', function (req, res) {
+    router.get("/", function (req, res) {
       res.json({
-        message: 'Welcome !'
+        message: "Welcome !"
       });
     });
 
-    router.route('/trans')
-      .post(function (req, res) {
-        var casht = new Details();
-        casht.bname = req.body.bname,
-          casht.pno = req.body.pno,
-          casht.date = req.body.date,
-          casht.transactiontype = req.body.transactiontype,
-          casht.amount = req.body.amount,
-          casht.transmode = req.body.transmode
+    router.route("/trans").post(function (req, res) {
+      var casht = new Details();
+      (casht.bname = req.body.bname),
+      (casht.pno = req.body.pno),
+      (casht.date = req.body.date),
+      (casht.transactiontype = req.body.transactiontype),
+      (casht.amount = req.body.amount),
+      (casht.transmode = req.body.transmode);
 
-        dbo.collection("transt").insertOne(casht, function (err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
-        });
-      })
-  });
+      dbo.collection("transt").insertOne(casht, function (err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+      });
+    });
+  }
+);
 
 // Fire up server
 app.listen(port);
 
 // print friendly message to console
-console.log('Server listening on port ' + port);
+console.log("Server listening on port " + port);
