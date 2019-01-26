6e5fd9f486b8e6d7bbcefac5b88d056218e2cb7c
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-01-26 23:29:55 +0530

    journals database connect first try

--- server.js
+++ server.js
@@ -52,10 +52,40 @@ MongoClient.connect(
       (casht.amount = req.body.amount),
       (casht.transmode = req.body.transmode);
 
+      var value1 = casht.transactiontype;
+      var value2 = casht.transmode;
+      var value3 = casht.date;
+      var value4 = casht.amount;
+      var value5 = casht.bname;
+      var value6 = casht.pno;
+
       dbo.collection("transt").insertOne(casht, function (err, res) {
         if (err) throw err;
         console.log("1 document inserted");
         db.close();
+      });
+
+      dbo.collection("journals").find({}).toArray(function (err, result) {
+        if (err) throw err;
+        for (i = 0; i < result.length; i++) {
+          if (result[i].transactiontype == casht.transactiontype && result[i].transmode == casht.transmode) {
+            var myorg = {
+              transactiontype: value1,
+              transmode: value2
+            }
+            var mynew = {
+              transactiontype: value1,
+              transmode: value2,
+              date: value3,
+              amount: value4,
+              banme: value5,
+              pno: value6,
+            }
+            abo.collection("journals").update(myorg, mynew, {
+              upsert: true
+            })
+          }
+        }
       });
     });
   }
